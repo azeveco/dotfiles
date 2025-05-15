83b9446c2601fd7e8b13efe1cf2e7e994c85bb85
Gabriel Azevedo <gazeveco@gmail.com>
2025-05-15 19:03:30 -0300

    Update to harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,3 +1,25 @@
+local harpoon_extensions = require("harpoon.extensions")
+
+-- Extension to highlight the current file, if it is opened
+require("harpoon"):extend(harpoon_extensions.builtins.highlight_current_file())
+
+-- Set keys to open harpooned files in split or new tab
+require("harpoon"):extend({
+  UI_CREATE = function(cx)
+    vim.keymap.set("n", "<C-v>", function()
+      require("harpoon").ui:select_menu_item({ vsplit = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-x>", function()
+      require("harpoon").ui:select_menu_item({ split = true })
+    end, { buffer = cx.bufnr })
+
+    vim.keymap.set("n", "<C-t>", function()
+      require("harpoon").ui:select_menu_item({ tabedit = true })
+    end, { buffer = cx.bufnr })
+  end,
+})
+
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -11,7 +33,7 @@ return {
       desc = "Harpoon File",
     },
     {
-      "<leader>H",
+      "<C-e>",
       function()
         local harpoon = require("harpoon")
         harpoon.ui:toggle_quick_menu(harpoon:list())
