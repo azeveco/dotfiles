b9eff640bccc588f4848f8723d935eafd4b73ada
Gabriel Azevedo <gazeveco@gmail.com>
2025-05-15 22:46:10 -0300

    Adds Edgy

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    -- event = "VeryLazy",
+  },
+}
+
