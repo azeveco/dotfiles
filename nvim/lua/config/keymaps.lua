75c5b9d59b26993c86f53da0269b0fbd485f4f77
Gabriel Azevedo <gazeveco@gmail.com>
2025-05-17 02:46:46 -0300

    keymap to move to 0 and $ easily

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,6 +12,16 @@ keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 keymap("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 keymap("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
+-- Easier access to beginning and end of lines
+keymap("n", "<A-h>", "^", {
+  desc = "Go to start of line",
+  silent = true,
+})
+keymap("n", "<A-l>", "$", {
+  desc = "Go to end of line",
+  silent = true,
+})
+
 -- Keymap to do a search and replace
 keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "SR" })
 
