26cf223f356a2b090e673746f2e306c96d3f325a
Gabriel Azevedo <gazeveco@gmail.com>
2025-09-29 22:16:40 -0300

    add new themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -6,4 +6,23 @@ return {
       -- colorscheme = "catppuccin-mocha",
     },
   },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-storm",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+  },
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   require("bamboo").setup({})
+    --   require("bamboo").load()
+    -- end,
+  },
 }
