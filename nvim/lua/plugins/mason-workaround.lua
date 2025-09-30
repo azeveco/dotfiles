49060e1fc5c74fae401801522469249bbda3b97d
Gabriel Azevedo <gazeveco@gmail.com>
2025-09-29 22:13:19 -0300

    removes mason workaround

--- nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,9 +0,0 @@
--- return {
---   { "mason-org/mason.nvim", version = "1.11.0" },
---   { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
--- }
-
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
