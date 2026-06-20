ff5cb61b570ebf2ab80de496487b0d1fa4fd0041
Gabriel Azevedo <gazeveco@gmail.com>
2026-06-20 18:09:00 -0300

    scopes gitsigns keymaps to <leader>gh

--- nvim/lua/azeveco/plugins/gitsigns.lua
+++ nvim/lua/azeveco/plugins/gitsigns.lua
@@ -42,19 +42,19 @@ require('gitsigns').setup {
 
     -- Git / Hunks
     -- Stage, reset, preview, and inspect hunks from both visual and normal mode.
-    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git stage hunk' })
-    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git reset hunk' })
-    map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git stage hunk' })
-    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git reset hunk' })
-    map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git Stage buffer' })
-    map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git Reset buffer' })
-    map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git preview hunk' })
-    map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk inline' })
-    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = 'git blame line' })
-    map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git diff against index' })
-    map('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git Diff against last commit' })
-    map('n', '<leader>hQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk Quickfix list (all files in repo)' })
-    map('n', '<leader>hq', gitsigns.setqflist, { desc = 'git hunk quickfix list (all changes in this file)' })
+    map('v', '<leader>ghs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git stage hunk' })
+    map('v', '<leader>ghr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = 'git reset hunk' })
+    map('n', '<leader>ghs', gitsigns.stage_hunk, { desc = 'git stage hunk' })
+    map('n', '<leader>ghr', gitsigns.reset_hunk, { desc = 'git reset hunk' })
+    map('n', '<leader>ghS', gitsigns.stage_buffer, { desc = 'git Stage buffer' })
+    map('n', '<leader>ghR', gitsigns.reset_buffer, { desc = 'git Reset buffer' })
+    map('n', '<leader>ghp', gitsigns.preview_hunk, { desc = 'git preview hunk' })
+    map('n', '<leader>ghi', gitsigns.preview_hunk_inline, { desc = 'git preview hunk inline' })
+    map('n', '<leader>ghb', function() gitsigns.blame_line { full = true } end, { desc = 'git blame line' })
+    map('n', '<leader>ghd', gitsigns.diffthis, { desc = 'git diff against index' })
+    map('n', '<leader>ghD', function() gitsigns.diffthis '@' end, { desc = 'git Diff against last commit' })
+    map('n', '<leader>ghQ', function() gitsigns.setqflist 'all' end, { desc = 'git hunk Quickfix list (all files in repo)' })
+    map('n', '<leader>ghq', gitsigns.setqflist, { desc = 'git hunk quickfix list (all changes in this file)' })
 
     -- UI
     -- Toggle blame and intra-line diff helpers when you want more git context.
